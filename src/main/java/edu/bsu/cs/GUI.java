df7d7e14c716e732af6af256a36b0852dab74c49
Timothy <quiksylvir@gmail.com>
2024-10-04 10:38:47 -0400

    Fix fxmlSample.fxml to point to GUI

--- src/main/java/edu/bsu/cs/GUI.java
+++ src/main/java/edu/bsu/cs/GUI.java
@@ -1,24 +1,41 @@
 package edu.bsu.cs;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.util.Objects;
 
 
 public class GUI extends Application {
+
+
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("fxmlSample.fxml")));
-        primaryStage.setTitle("D&D Loot Generator");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
+    public void start(Stage primaryStage) throws Exception {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("fxmlSample.fxml")));
+            primaryStage.setTitle("D&D Loot Generator");
+            primaryStage.setScene(new Scene(root));
+            primaryStage.show();
     }
+
+    @FXML
+    public TextArea textArea;
+    public Button button;
+
+    @FXML
+    public void setText(ActionEvent actionEvent) {
+        textArea.setText("Hello");
+    }
+
 }
