bed8aa3160b051691a295205c0560a087db68f34
Alec <168141387+alecthedev@users.noreply.github.com>
2024-10-09 12:01:24 -0400

    Add TestAPIConnection

---
+++ src/test/java/edu/bsu/cs/TestAPIConnection.java
@@ -0,0 +1,41 @@
+package edu.bsu.cs;
+
+import com.jayway.jsonpath.JsonPath;
+import net.minidev.json.JSONArray;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.net.URLConnection;
+import java.nio.charset.Charset;
+import java.util.Objects;
+
+public class TestAPIConnection {
+
+    @Test
+    public void TestConnectToAPI() throws URISyntaxException, IOException {
+        URLConnection urlConnection = APIConnection.connectToAPI("");
+        Assertions.assertNotNull(urlConnection);
+    }
+
+    @Test
+    public void TestConnectionReturnsJson() throws URISyntaxException, IOException {
+        URLConnection urlConnection = APIConnection.connectToAPI("");
+        String stringifiedJson = readJsonSampleAsString(urlConnection);
+        getJsonSample(stringifiedJson);
+        Assertions.assertInstanceOf(JSONArray.class, getJsonSample(stringifiedJson));
+    }
+
+    private String readJsonSampleAsString(URLConnection connection) throws IOException {
+        return new String(connection.getInputStream().readAllBytes(), Charset.defaultCharset());
+    }
+
+    private JSONArray getJsonSample(String stringifiedJson) {
+        return JsonPath.read(stringifiedJson, "$..*");
+    }
+
+
+
+}
