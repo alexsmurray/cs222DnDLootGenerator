741e1967a500cd08a9b3b52aa726d0d2576ca66d
Timothy <quiksylvir@gmail.com>
2024-10-14 11:30:41 -0400

    Implement JsonReader to return file info.

--- src/main/java/edu/bsu/cs/JsonFileReader.java
+++ src/main/java/edu/bsu/cs/JsonFileReader.java
@@ -1,5 +1,11 @@
 package edu.bsu.cs;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class JsonFileReader {
 
+    public static String readFileToString(String filePath) throws IOException {
+        return JsonToString.readJsonAsString(new FileInputStream(filePath));
+    }
 }
--- src/test/java/edu/bsu/cs/TestJsonFileReader.java
+++ src/test/java/edu/bsu/cs/TestJsonFileReader.java
@@ -5,16 +5,21 @@ import org.junit.jupiter.api.Test;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.util.Scanner;
 
 public class TestJsonFileReader {
     @Test
-    public void testSampleFileIsNotBlank() throws IOException {
+    public void testSampleFileRead() throws IOException {
         StringWriter stringWriter = new StringWriter();
         FileInputStream testFileInputStream = new FileInputStream("src/test/resources/SampleMagicItemPage1.json");
         InputStreamReader inputStreamReader = new InputStreamReader(testFileInputStream, StandardCharsets.UTF_8);
         inputStreamReader.transferTo(stringWriter);
         String result = stringWriter.toString().replaceAll("\\r\\n?", "\n");
         Assertions.assertEquals(result, SampleMagicString.SAMPLE);
+    }
+
+    @Test
+    public void testFileIsNotBlank() throws IOException {
+        String result = JsonFileReader.readFileToString("src/main/resources/magicitems.txt");
+        Assertions.assertFalse(result.isBlank());
     }
 }
