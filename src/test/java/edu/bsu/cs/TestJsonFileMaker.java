8cdba0250f6f4b4a880f33510b6a119056b1e36b
Alec <168141387+alecthedev@users.noreply.github.com>
2024-10-15 14:47:21 -0400

    Refactor TestJsonFileMaker to use JsonFileReader instead of FileReader

--- src/test/java/edu/bsu/cs/TestJsonFileMaker.java
+++ src/test/java/edu/bsu/cs/TestJsonFileMaker.java
@@ -3,7 +3,6 @@ package edu.bsu.cs;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.io.FileReader;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
@@ -13,18 +12,8 @@ public class TestJsonFileMaker {
     public void testWriteItemsJsonToFile() throws IOException, URISyntaxException {
         // We only test this method with one category to avoid 30+ API calls when testing
         JsonFileMaker fileMaker = new JsonFileMaker();
-        String fileContents;
         fileMaker.writeItemsJsonToFile("weapons");
-        try (FileReader fileReader = new FileReader("src/main/resources/weapons.txt")) {
-            StringBuilder fileStringBuilder = new StringBuilder();
-            int i = fileReader.read();
-            while(i != -1) {
-                fileStringBuilder.append((char)i);
-
-                i = fileReader.read();
-            }
-            fileContents = fileStringBuilder.toString();
-        }
+        String fileContents = JsonFileReader.readFileToString("src/main/resources/weapons.txt");
         Assertions.assertFalse(fileContents.isBlank());
     }
 
