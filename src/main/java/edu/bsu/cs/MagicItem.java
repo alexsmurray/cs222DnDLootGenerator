f6a52cdf621f15e54a1c4859d06afe1cb0822509
Alec <168141387+alecthedev@users.noreply.github.com>
2024-10-15 10:42:45 -0400

    Add MagicItem class

---
+++ src/main/java/edu/bsu/cs/MagicItem.java
@@ -0,0 +1,36 @@
+package edu.bsu.cs;
+
+public class MagicItem extends Item {
+
+    private final String name;
+    private final String rarity;
+    private final String type;
+    private final boolean attunement;
+
+    public MagicItem(String name, String rarity, String type, boolean attunement) {
+        this.name = name;
+        this.rarity = rarity;
+        this.type = type;
+        this.attunement = attunement;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getRarity() {
+        return rarity;
+    }
+
+    @Override
+    public String getType() {
+        return type;
+    }
+
+    @Override
+    public boolean getAttunement() {
+        return attunement;
+    }
+}
