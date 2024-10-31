bd785244955ae5126500533df58f6d374a074172
alexander murray <alexander.murray@bsu.edu>
2024-10-31 10:25:25 -0400

    Remove unnecessary constructors for Weapon

--- src/main/java/edu/bsu/cs/Weapon.java
+++ src/main/java/edu/bsu/cs/Weapon.java
@@ -4,7 +4,7 @@ import java.util.Hashtable;
 
 public class Weapon extends Item {
     Dictionary<String, String> statDictionary = new Hashtable<>();
-    public Weapon(String name, String rarity, String type, String attunement, String details, String isVersitile, String isMartial, String isMelee, String rangedAttackPossible, String hasReach, String distanceUnit, String damageDice, String versitileDice, String reach, String range, String longRange, String isFinesse, String isThrown, String isTwoHanded, String requiresAmmunation, String requiresLoading, String isHeavy, String isLight, String isLance, String isNet, String isSimple, String isImprovised) {
+    public Weapon(String name, String rarity, String type, String attunement, String details) {
         super(name, rarity, type, attunement, details);
     }
 
