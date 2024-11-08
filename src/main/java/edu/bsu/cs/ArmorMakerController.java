4dcc93f358fe97a1a43790071078cc1342baff32
alexander murray <alexander.murray@bsu.edu>
2024-11-08 13:28:28 -0500

    Adjust ArmorMaker GUI layout and add attunement and stealth disadvantage toggle buttons.

--- src/main/java/edu/bsu/cs/ArmorMakerController.java
+++ src/main/java/edu/bsu/cs/ArmorMakerController.java
@@ -7,10 +7,18 @@ public class ArmorMakerController {
     public TextField armorNameInput;
     public Pane armorMaker;
     public ChoiceBox<String> dexModChoice;
-    public RadioButton stealthDisadvantageRadio;
-    public ToggleGroup stealthDisadvantage;
-    public RadioButton noneRadio;
     public TextArea armorDescription;
     public ChoiceBox<String> armorRarityChoice;
     public ChoiceBox<String> categoryChoice;
+    public ToggleButton attunementToggle;
+    public Label requiresAttunementLabel;
+    public Label stealthDisadvantageLabel;
+    public ToggleButton stealthDisadvantageToggle;
+
+    public void checkAttunement() {
+        requiresAttunementLabel.setVisible(attunementToggle.isSelected());
+    }
+    public void checkStealthDisadvantage() {
+        stealthDisadvantageLabel.setVisible(stealthDisadvantageToggle.isSelected());
+    }
 }
