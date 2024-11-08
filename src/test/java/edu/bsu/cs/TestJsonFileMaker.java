c5ad99a87221378e91c8b8a2aea79133b1cc1868
Timothy <quiksylvir@gmail.com>
2024-11-08 11:26:32 -0500

    Fix issue where subfolders would not be present

--- src/test/java/edu/bsu/cs/TestJsonFileMaker.java
+++ src/test/java/edu/bsu/cs/TestJsonFileMaker.java
@@ -5,11 +5,15 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class TestJsonFileMaker {
 
     @Test
     public void testWriteItemsJsonToFile() throws IOException, URISyntaxException {
+        Files.createDirectories(Paths.get("src/main/resources/dataFiles"));
+
         JsonFileMaker fileMaker = new JsonFileMaker();
         fileMaker.writeItemsJsonToFile("weapons");
         String fileContents = JsonFileReader.readFileToString("src/main/resources/dataFiles/weapons.txt");
