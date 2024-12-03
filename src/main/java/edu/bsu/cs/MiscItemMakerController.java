74e7a46a9d6db742093e0b5023fa44de9bec70aa
James <jarjrjarsr@gmail.com>
2024-12-03 11:31:22 -0500

    Add write misc items to file

--- src/main/java/edu/bsu/cs/MiscItemMakerController.java
+++ src/main/java/edu/bsu/cs/MiscItemMakerController.java
@@ -3,6 +3,8 @@ package edu.bsu.cs;
 import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
 
+import java.io.IOException;
+
 public class MiscItemMakerController {
     public Pane armorMaker;
     public TextField itemNameInput;
@@ -15,4 +17,32 @@ public class MiscItemMakerController {
     public void checkAttunement() {
         requiresAttunementLabel.setVisible(attunementToggle.isSelected());
     }
+
+    public String collectMiscItemDetails(){
+        String[] checkedInputs = {itemNameInput.getText()};
+        if (!ErrorHandler.verifyHomebrewInputsNotBlank(checkedInputs)) {
+            GUI.displayHomebrewWeaponFieldsAlert();
+            return "";
+        }
+        return "{\n" +
+                "\t\"Item_Type\": \""+ itemTypeChoice.getValue() + " HB\",\n" +
+                "\t\"Name\": \"" + itemNameInput.getText() + "\",\n" +
+                "\t\"Rarity\": \"" + rarityChoice.getValue() + "\",\n" +
+                "\t\"Attunement\": " + attunementToggle.isSelected() + ",\n" +
+                "\t\"Description\":" +
+                "\n\n\"" + itemDescription.getText() + "\"\n" +
+                "},]\n" +
+                "}";
+    }
+
+    public void writeMiscItemToFile() throws IOException {
+        JsonFileMaker jsonFileMaker = new JsonFileMaker();
+        String itemDetails = collectMiscItemDetails();
+        if (!itemDetails.isEmpty()) {
+            jsonFileMaker.writeHomebrewToFile(itemDetails);
+            GUI.displayItemCreatedAlert();
+        }
+        //clearAllInput();
+    }
+
 }
