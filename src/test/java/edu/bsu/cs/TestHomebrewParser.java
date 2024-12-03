6f67f718698e9086c63fc1e83391d3637cafc05e
James <jarjrjarsr@gmail.com>
2024-12-03 11:40:17 -0500

    Add tests for Homebrew Parser

---
+++ src/test/java/edu/bsu/cs/TestHomebrewParser.java
@@ -0,0 +1,44 @@
+package edu.bsu.cs;
+
+import net.minidev.json.JSONArray;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.Hashtable;
+
+public class TestHomebrewParser {
+    HomebrewItemParser homebrewItemParser = new HomebrewItemParser(JsonFileReader.readFileToString("src/main/resources/dataFiles/homebrew.txt"));
+
+    public TestHomebrewParser() throws IOException {
+    }
+
+    @Test
+    public void testBuildJsonArrayOfHomebrewItemsNames(){
+        JSONArray testArray = homebrewItemParser.buildJsonArrayOfHomebrewItemsNames();
+
+        Assertions.assertFalse(testArray.isEmpty());
+    }
+
+    @Test
+    public void testBuildJsonArrayOfHomebrewItems(){
+        String testRarity = homebrewItemParser.buildJsonArrayOfHomebrewItems("Rarity").getFirst().toString();
+
+        Assertions.assertEquals("Non-existent",testRarity);
+    }
+
+    @Test
+    public void testParseHomebrewAttribute(){
+        String testRarity = homebrewItemParser.parseHomebrewAttribute("Description",0);
+
+        Assertions.assertEquals("YOU SHOULD NOT BE HERE",testRarity);
+    }
+
+    @Test
+    public void testParseAllHomebrewItemDetails(){
+        Hashtable<String, String> testList = homebrewItemParser.parseAllHomebrewItemDetails(0);
+
+        Assertions.assertNull(testList.get("Name"));
+    }
+
+}
